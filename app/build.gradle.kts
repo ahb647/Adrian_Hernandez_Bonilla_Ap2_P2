9923b43019dacf94c33d27f7e0d2fd149430b23c
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-10-29 20:02:11 -0400

    Base para el examen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    kotlin("plugin.serialization")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -56,4 +60,56 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    //room
+    implementation("androidx.room:room-runtime:2.7.2")
+    annotationProcessor("androidx.room:room-compiler:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2")
+    //  optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:2.7.2")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.57.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
+    implementation("androidx.compose.runtime:runtime-livedata:1.9.0")
+    implementation("androidx.compose.runtime:runtime:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.3")
+
+    //navegacion
+    implementation("androidx.navigation:navigation-compose:2.8.0-rc01")
+
+// Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    // --- API / Red ---
+    // Retrofit (Cliente HTTP)
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    // Conversor para Kotlinx Serialization
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    // OkHttp (Cliente HTTP subyacente)
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
+    // Logging interceptor (para ver peticiones/respuestas)
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
+
+    // --- Kotlin Serialization ---
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    // --- Moshi (JSON) ---
+    implementation("com.squareup.moshi:moshi:1.15.1")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
+
+    // Para recyclerview
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // Iconos de Material
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
+    //Material3
+    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
+    implementation("androidx.compose.material:material-icons-extended:1.6.4")
 }
