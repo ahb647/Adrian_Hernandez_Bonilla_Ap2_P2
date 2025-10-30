2aef5e7d4ff7f8c419cce4b151aac5b26c020823
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-10-29 20:25:52 -0400

    Configurando Apimodule para el examen

---
+++ app/src/main/java/edu/ucne/adrian_hernandez_bonilla_ap2_p2/app/data/di/ApiModule.kt
@@ -0,0 +1,51 @@
+package edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.data.di
+
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    private const val BASE_URL = "Url de tu API aquí"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(moshi: Moshi, client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.data.di.ApiModule.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(client)
+            .build()
+
+
+
+}
