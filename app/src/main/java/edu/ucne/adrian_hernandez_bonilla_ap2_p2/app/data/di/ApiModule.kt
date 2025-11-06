5e64f5e08af133f4f940ff04c119ba2679c562e8
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-11-05 21:29:00 -0400

    Configurando ApiModule para gestionar la conexión a la API y los repositorios

--- app/src/main/java/edu/ucne/adrian_hernandez_bonilla_ap2_p2/app/data/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/adrian_hernandez_bonilla_ap2_p2/app/data/di/ApiModule.kt
@@ -1,25 +1,29 @@
 package edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.data.di
 
-
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.data.remote.GestionGastosApi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
+import java.util.concurrent.TimeUnit
+import edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.data.repository.GestionGastosRepositoryApi
+import edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.domain.repository.GastosRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
 
-
-
-    private const val BASE_URL = "Url de tu API aquí"
+    @Provides
+    @Named("baseUrl")
+    fun provideBaseUrl(): String = "https://gestionhuacalesapi.azurewebsites.net/"
 
     @Provides
     @Singleton
@@ -36,18 +40,34 @@ object ApiModule {
         }
         return OkHttpClient.Builder()
             .addInterceptor(logging)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
             .build()
     }
 
     @Provides
     @Singleton
-    fun provideRetrofit(moshi: Moshi, client: OkHttpClient): Retrofit =
+    fun provideRetrofit(
+        @Named("baseUrl") baseUrl: String,
+        moshi: Moshi,
+        client: OkHttpClient
+    ): Retrofit =
         Retrofit.Builder()
-            .baseUrl(edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.data.di.ApiModule.BASE_URL)
+            .baseUrl(baseUrl)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .client(client)
             .build()
 
+    @Provides
+    @Singleton
+    fun provideGestionGastosApi(retrofit: Retrofit): GestionGastosApi =
+        retrofit.create(GestionGastosApi::class.java)
 
 
+    @Provides
+    @Singleton
+    fun provideGastosRepository(
+        impl: GestionGastosRepositoryApi
+    ): GastosRepository = impl
 }
