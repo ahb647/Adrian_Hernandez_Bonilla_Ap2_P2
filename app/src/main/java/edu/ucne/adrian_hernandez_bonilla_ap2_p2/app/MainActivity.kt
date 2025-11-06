b09bc521abfdb581929fe143b9c422338400479e
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-11-05 21:31:38 -0400

    Añadiendo navegación a la aplicación con NavHost y RegistroGastosScreen en MainActivity

--- app/src/main/java/edu/ucne/adrian_hernandez_bonilla_ap2_p2/app/MainActivity.kt
+++ app/src/main/java/edu/ucne/adrian_hernandez_bonilla_ap2_p2/app/MainActivity.kt
@@ -5,43 +5,43 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.ui.theme.Adrian_Hernandez_Bonilla_Ap2_P2Theme
+import edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.ui.theme.registrosGastos.RegistroGastosScreen
+import edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.ui.theme.util.Route
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Adrian_Hernandez_Bonilla_Ap2_P2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                AppNavigation(navController = navController)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigation(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Route.Gastos_List,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        composable(Route.Gastos_List) {
+            RegistroGastosScreen(
+                onNavigateBack = { navController.popBackStack() }
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Adrian_Hernandez_Bonilla_Ap2_P2Theme {
-        Greeting("Android")
     }
 }
