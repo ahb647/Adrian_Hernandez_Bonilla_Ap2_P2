e0f5f4b303d3d116d85e5dc633a925d756bb6b00
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-11-05 21:30:15 -0400

    Añadiendo clase de datos GastosResponsedto para la serialización de respuestas de gastos

---
+++ app/src/main/java/edu/ucne/adrian_hernandez_bonilla_ap2_p2/app/data/remote/dto/GastosResponsedto.kt
@@ -0,0 +1,16 @@
+package edu.ucne.adrian_hernandez_bonilla_ap2_p2.app.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GastosResponsedto(
+
+    val gastoId: Int? = null,
+    val fecha: String? = null,
+    val suplidor: String,
+    val ncf: String,
+    val itbis: Double? = null,
+    val monto: Double? = null
+
+
+)
